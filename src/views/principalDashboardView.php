e89200b6ddb904b3341bf55f4d71e87743de48ae
MartinFitS <sernamartin46@gmail.com>
2024-03-06 07:23:37 -0600

    first version of feed

--- src/views/principalDashboardView.php
+++ src/views/principalDashboardView.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../styles/publisStyles.css">
+    <link rel="stylesheet" href="../styles/modalCrearPublicacion.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -50,9 +51,9 @@
   </div>
 
   <div class="centralSide">
-    <div class="divCardCrearPubli">
+    <div id="create-publi_form" class="divCardCrearPubli">
       <div class="imgDivCrearPubli">
-      <img src="https://b2472105.smushcdn.com/2472105/wp-content/uploads/2023/01/Perfil-Profesional-_63-819x1024.jpg?lossy=1&strip=1&webp=1" alt="">
+      <img class="centralSideImageImage" src="https://b2472105.smushcdn.com/2472105/wp-content/uploads/2023/01/Perfil-Profesional-_63-819x1024.jpg?lossy=1&strip=1&webp=1" alt="">
       </div>
       <div class="ipDivCrearPubli">
       <input type="text" placeholder="Haz una publicación">
@@ -60,13 +61,79 @@
 
 
     </div>
+
+    <div class="publisSection">
+      <div class="publiCard">
+        <div class="datosPubli">
+          <div class="fotoDatosPubli"><img src="https://b2472105.smushcdn.com/2472105/wp-content/uploads/2023/01/Perfil-Profesional-_63-819x1024.jpg?lossy=1&strip=1&webp=1" alt=""></div>
+          <div class="datosDatosPubli">
+            <h1>Janice Griffith</h1>
+            <p>Published: June,2 2018 19:PM</p>
+          </div>
+          </div>
+          <div class="descriptionPublicacion">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis omnis beatae, ipsum sunt aliquam quasi magni ullam tenetur officia consequuntur. Odit officiis ex a ipsam ab esse nihil quia voluptates.</p>
+          </div>
+          <div class="fotoPubliPrincipal">
+            <img class="imagenPublicacionPrincipal" src="https://webunwto.s3.eu-west-1.amazonaws.com/2021-01/un-general-assembly.jpg?VersionId=WCBuWMPVkL.WYPHiNRQP2lHM3.AhwcKv" alt="">
+          </div>
+
+          <div class="reaccionesPubliBar">
+              <li><img src="../img/reaction-icons/001-como.png" alt="">10</li>
+              <li><img src="../img/reaction-icons/002-no-me-gusta.png" alt="">550</li>
+              <li><img src="../img/reaction-icons/003-comentario-positivo.png" alt="">100</li>
+            </ul>
+        </div>
+      </div>
+    </div>
   </div>
 
   <div class="rigthSide">
 
   </div>
+
+
+  <div id="modal_publis" class="modalCrearPublicacion">
+      <div class="modalContentForm">
+        <div class="closeAndTitle">
+          <div class="titleModal">Crear Publicación</div>
+          <div class="closeModal">X</div>
+        </div>
+
+        <div class="modalUserInfo">
+          <div class="imagenUserModal"><img src="" alt=""></div>
+          <div class="infoNameModal">
+            <h1>Joseph Joestar</h1>
+          </div>
+        </div>
+
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+
+
+        <div class="sectionImagenesModal">
+          <div class="btnAddImagen"></div>
+          <div class="previsualizacionImagen"></div>
+        </div>
+
+
+        <div class="publicarBotonModal">
+          <button>Publicar</button>
+        </div>
+
+      </div>
+  </div>
 </div>
-  
+  <script>
+    const form = document.getElementById("create-publi_form")
+
+    form.addEventListener("click", (e)=> {
+      e.preventDefault();
+
+      document.getElementById("modal_publis").style.display = 'block';
+
+      console.log("Hola")
+    })
+  </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
