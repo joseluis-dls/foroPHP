f3ba28f64a49b528b8e1e9fc5c783be9c92b9954
MartinFitS <sernamartin46@gmail.com>
2024-03-08 10:32:00 -0600

    maquetacion feed y admin dashboard

--- src/views/principalDashboardView.php
+++ src/views/principalDashboardView.php
@@ -12,12 +12,12 @@
 
 <body>
 
-  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top" style="    box-shadow: -20px 40px 40px 10px rgba(0, 0, 0, 0.3);">
-    <div class="container-fluid">
-      <!-- Columna izquierda: logo o título -->
-      <div class="col-md-4">
-        <a class="navbar-brand" href="#">UNESforum</a>
-      </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top" style="    box-shadow: 5px -9px 20px 8px rgba(0, 0, 0, 0.3);">
+  <div class="container-fluid">
+    <!-- Columna izquierda: logo o título -->
+    <div class="col-md-4">
+      <a class="navbar-brand" href="#">UNESforum</a>
+    </div>
 
       <!-- Columna central: buscador -->
       <div class="col-md-4 text-center"> <!-- Agregado text-center -->
@@ -27,20 +27,26 @@
         </form>
       </div>
 
-      <!-- Columna derecha: iconos de notificaciones, configuración y cerrar sesión -->
-      <div class="col-md-4 text-end">
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-bell">Notificaciones</i></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-user-cog">Cuenta</i></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../exit.php"><i class="fas fa-sign-out-alt">Cerrar Sesión</i></a>
-          </li>
-        </ul>
-      </div>
+    <!-- Columna derecha: iconos de notificaciones, configuración y cerrar sesión -->
+    <div class="col-md-4 text-end">
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item me-3 " >
+          <a class="nav-link " href="#"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+  <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+</svg></a>
+        </li>
+        <li class="nav-item me-3 li-Navbar_animacion">
+          <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+</svg></a>
+        </li>
+        <li class="me-3 nav-item li-Navbar_animacion">
+          <a class="nav-link" href="../exit.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+  <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+</svg></a>
+        </li>
+      </ul>
     </div>
   </nav>
 
