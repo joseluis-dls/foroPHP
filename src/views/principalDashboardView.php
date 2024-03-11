cbd485b84e979cc51dbbb279c800a636303dace9
Francisco <frodriguez25@ucol.mx>
2024-03-11 16:16:56 -0600

    hotfix

--- src/views/principalDashboardView.php
+++ src/views/principalDashboardView.php
@@ -262,7 +262,6 @@
               src="https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
               alt=""></div>
         </div>
-|
         <div class="btn_publicar">
           <button id="btn_publicar">Publicar</button>
         </div>
