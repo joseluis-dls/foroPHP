903ec153dc0dde7bca321f5a634700bab0d03391
Francisco <frodriguez25@ucol.mx>
2024-03-21 09:49:24 -0600

    lst sht

--- src/views/principalDashboardView.php
+++ src/views/principalDashboardView.php
@@ -374,23 +374,30 @@ if (mysqli_num_rows($result) > 0) {
               posts.forEach(post => {
                 template += `
                   <div class="publiCard">
-                    <div class="datosPubli">
+                `;
+                if (post.session_id == post.user_id) {
+                  template += `
+                  <div class="datosPubli">
                       <div class="fotoDatosPubli"><img src="${post.profile_photo}" alt=""></div>
                       <div class="datosDatosPubli">
                         <h1>${post.username}</h1>
                         <p>Published: ${post.posted_at}</p>
-                `;
-                if (post.session_id == post.user_id) {
-                  template += `
-                        <button id="btn_delete" data-post-id="${post.post_id}">Delete</button>
                       </div>
-                    </div>
+<button id="btn_delete" class="btn_delete" data-post-id="${post.post_id}"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg></button>                    </div>
                     <div class="descriptionPublicacion">
                       <p>${post.post_content}</p>
                     </div>
                   `
                 } else {
                   template += `
+                  <div class="datosPubli">
+                      <div class="fotoDatosPubli"><img src="${post.profile_photo}" alt=""></div>
+                      <div class="datosDatosPubli">
+                        <h1>${post.username}</h1>
+                        <p>Published: ${post.posted_at}</p>
                           </div>
                         </div>
                         <div class="descriptionPublicacion">
